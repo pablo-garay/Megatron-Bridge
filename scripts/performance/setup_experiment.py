e87cef096f5a321c3a4c95a4ea8df4aaba928dac
Sanju C Sudhakaran <scsudhakaran@nvidia.com>
2025-09-18 12:06:17 +0530

    Workaround for installation failures for login-node

--- scripts/performance/setup_experiment.py
+++ scripts/performance/setup_experiment.py
@@ -121,4 +121,12 @@ if __name__ == "__main__":
         args=target_script_args,
     )
 
+    exp_name = f"{args.model_name}_{args.model_size}_{args.domain}_{args.task}"
+    exp_name += "_bf16" if args.compute_dtype == "bf16" else f"_{args.compute_dtype}_{args.fp8_recipe}"
+
+    # workaround: update the experiment name to align LLMB naming convention
+    train_config =  yaml_overrides_omega["perf_matrix"][args.gpu][f"num_gpus_{args.num_gpus}"]["common"]
+    exp_config = f"gpus{args.num_gpus}_tp{train_config["tp"]}_pp{train_config["pp"]}_cp{train_config["cp"]}_vp{train_config["vp"]}_ep{train_config["ep"]}_mbs{train_config["mbs"]}_gbs{train_config["gbs"]}"
+    exp_name = f"pretrain_{args.model_name}_{args.model_size}_{args.compute_dtype}_{exp_config}"
+
     run.run(train_script, executor=executor, plugins=plugins, dryrun=args.dryrun, detach=True, name=exp_name)
