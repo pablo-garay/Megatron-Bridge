2e15bf259115185432e092ac21c1d444c8ff0111
Chen Cui <chcui@nvidia.com>
2025-10-06 16:21:21 -0700

    pipeline dtype
    Signed-off-by: Chen Cui <chcui@nvidia.com>

--- examples/conversion/hf_megatron_roundtrip_multi_gpu.py
+++ examples/conversion/hf_megatron_roundtrip_multi_gpu.py
@@ -96,6 +96,7 @@ def main(
                 "pipeline_model_parallel_size": pp,
                 "expert_model_parallel_size": ep,
                 "expert_tensor_parallel_size": etp,
+                "pipeline_dtype": torch.bfloat16,
             },
             wrap_with_ddp=False,
         )
