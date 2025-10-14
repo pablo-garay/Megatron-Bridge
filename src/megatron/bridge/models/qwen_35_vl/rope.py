d2f3d587d8c269d31a4d971e8d2ce2cbb16f9c25
ykarnati <ykarnati@nvidia.com>
2025-10-14 09:04:39 -0700

    add rope for language model

---
+++ src/megatron/bridge/models/qwen_35_vl/rope.py
@@ -0,0 +1,36 @@
+import torch
+from torch import Tensor
+from typing import List
+from transformers.models.qwen3_vl_moe.modeling_qwen3_vl_moe import Qwen3VLMoeTextRotaryEmbedding
+
+
+class Qwen3VLTextRotaryEmbedding(Qwen3VLMoeTextRotaryEmbedding):
+
+    def forward(self, position_ids: torch.Tensor, mrope_section: List[int]) -> Tensor:
+        """Forward pass of multimodal RoPE embedding.
+
+        Args:
+            position_ids (torch.Tensor): A postion_id tensor with shape [3, batchsize, seqlens]
+            mrope_section (list[int]): Multimodal rope section is for channel dimension of temporal,
+                height and width in rope calculation.
+
+        Returns:
+            Tensor: Embeddings after applying RoPE.
+        """
+        seq = position_ids.to(device=self.inv_freq.device, dtype=self.inv_freq.dtype)
+
+        if self.seq_len_interpolation_factor is not None:
+            seq *= 1 / self.seq_len_interpolation_factor
+
+        # shape (3, bs, dim, 1)
+        inv_freq_expanded = self.inv_freq[None, None, :, None].expand(3, seq.shape[1], -1, 1)
+        # shape (3, bs, 1, seq_length)
+        seq_expanded = seq[:, :, None, :].float()
+        # shape (3, bs, seq_length, dim)
+        freqs = (inv_freq_expanded @ seq_expanded).transpose(2, 3)
+        freqs = self.apply_interleaved_mrope(freqs, mrope_section)
+        emb = torch.cat((freqs, freqs), dim=-1)
+
+        # shape (seq_length, bs, 1, 2 * dim)
+        emb = emb[..., None, :].transpose(0, 1).contiguous()
+        return emb
